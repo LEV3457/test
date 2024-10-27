658e7222e4684eec24610c568e2661ef5f9ee3c1
LEV3457 <lukyanovivan83@gmail.com>
2024-10-27 18:10:57 +0000

    I have added calc

--- calc.py
+++ calc.py
@@ -1,3 +1,17 @@
-number1 = int(input("Введите число :")
-number2 = int(input("Введите число :")
-print(number1 + number2)
+number1 = int(input("Введите число :"))
+sign = input("Введите символ :")
+number2 = int(input("Введите число :"))
+if sign == "+":
+    print(number1 + number2)
+elif sign == "-":
+    if number1 > number2:
+        print(number1 - number2)
+    else:
+        print(number2 - number1)
+elif sign == "/" and number2 != 0:
+    print(number1 / number2)
+elif sign == "*":
+    print(number1 * number2)
+else:
+    print("Я не знаю такого знака")
+
